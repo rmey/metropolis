8169de7d81417fd00c969a6f022f0a87425af0c1
Rodi26 <30529563+Rodi26@users.noreply.github.com>
2019-06-19 14:27:26 +0200

    Update main.tf

--- cam-starterlibs/icd-mongodb/main.tf
+++ cam-starterlibs/icd-mongodb/main.tf
@@ -27,9 +27,6 @@ data "ibm_resource_group" "group" {
 }
 
 provider "ibm" {
-  bluemix_api_key    = "${var.ibm_bmx_api_key}"
-  softlayer_username = "${var.ibm_sl_username}"
-  softlayer_api_key  = "${var.ibm_sl_api_key}"
   bluemix_timeout = 24000
 }
 
