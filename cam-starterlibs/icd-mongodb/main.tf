27c2c8313fe2cb426deed5e7ca524e0cf07c7615
Rodi26 <30529563+Rodi26@users.noreply.github.com>
2019-06-19 15:45:01 +0200

    Update main.tf

--- cam-starterlibs/icd-mongodb/main.tf
+++ cam-starterlibs/icd-mongodb/main.tf
@@ -26,6 +26,10 @@ data "ibm_resource_group" "group" {
   name = "${var.rg}"
 }
 
+provider "ibm" {
+ ibmcloud_timeout = 30000
+}
+
 resource "ibm_database" "db" {
   resource_group_id = "${data.ibm_resource_group.group.id}"
   name              = "${var.svcname}"
@@ -44,9 +48,7 @@ resource "ibm_database" "db" {
     password = "${var.password}"
   }
 }
-provider “ibm” {
- ibmcloud_timeout = 30000
-}
+
 output "connectionString" {
   value = "http://${"${ibm_database.db.connectionstrings.0.composed}"}"
 }
