1ea87506fb7bc48480b155d3ba0939e5f1152355
Rene Meyer <rene.meyer@de.ibm.com>
2019-07-10 08:13:30 +0200

    ListView started

--- Helm-Examples/iks-appid-icd-todo-app/todo-app/frontend/client/src/components/TodoItem.js
+++ Helm-Examples/iks-appid-icd-todo-app/todo-app/frontend/client/src/components/TodoItem.js
@@ -1,29 +1,12 @@
+import React, {Component} from 'react';
+import Button from 'react-bootstrap/Button'
 
-/*
 class TodoItem extends React.Component {
   constructor(props) {
     super(props);
-    this.onClickClose = this.onClickClose.bind(this);
-    this.onClickDone = this.onClickDone.bind(this);
+    this.onClickRemove = this.onClickRemove.bind(this);
+    this.onClickComplete = this.onClickComplete.bind(this);
   }
-  onClickClose() {
-    var index = parseInt(this.props.index);
-    this.props.removeItem(index);
-  }
-  onClickDone() {
-    var index = parseInt(this.props.index);
-    this.props.markTodoDone(index);
-  }
-  render() {
-    var todoClass = this.props.item.done ?
-    "done" : "undone";
-    return (
-      React.createElement("li", { className: "list-group-item " },
-      React.createElement("div", { className: todoClass },
-      React.createElement("span", { className: "glyphicon glyphicon-ok icon", "aria-hidden": "true", onClick: this.onClickDone }),
-      this.props.item.value,
-      React.createElement("button", { type: "button", className: "close", onClick: this.onClickClose }, "\xD7"))));
 }}
 
 export default TodoItem;
-*/
