3129aae403e276bcbf0af50bf062d77f5aad1c92
Rene Meyer <rene.meyer@de.ibm.com>
2019-07-24 09:17:22 +0200

    fix Header/Footer

--- Helm-Examples/iks-appid-icd-todo-app/frontend/client/src/components/Header.js
+++ Helm-Examples/iks-appid-icd-todo-app/frontend/client/src/components/Header.js
@@ -10,12 +10,13 @@ export default class Header extends React.Component {
 
         //React.createElement("h1", null, "Welcome to your personal Todo List")
         <Navbar bg="primary" variant="dark">
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
+        <Navbar.Brand>Welcome to your Todo List</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+        <Navbar.Text>
+        Signed in as: <a href="#login">unknown</a>
+        </Navbar.Text>
+        </Navbar.Collapse>
         </Navbar>
     );
   }
@@ -24,16 +25,18 @@ export default class Header extends React.Component {
 export class Footer extends React.Component {
   render() {
     return(
-
-        //React.createElement("h1", null, "Welcome to your personal Todo List")
-        <Navbar bg="primary" variant="dark" fixed="bottom">
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Navbar>
+      //React.createElement("h1", null, "Welcome to your personal Todo List")
+      <Navbar bg="primary" variant="dark" fixed="bottom">
+      <Navbar.Text>
+      <a href="https://de.linkedin.com/in/ren%C3%A9-meyer-6271308" target="_blank">Created by René Meyer 2019</a>
+      </Navbar.Text>
+      <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-end">
+      <Navbar.Text>
+      <a href="https://github.com/rmey/metropolis/tree/master/Helm-Examples/iks-appid-icd-todo-app" target="_blank">Find project on Github</a>
+      </Navbar.Text>
+      </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
